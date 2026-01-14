5158fb4e63f9606ad35df54a591da52470c51611
Thomas Skerbis <thomas.skerbis@mac.com>
2026-01-14 22:16:19 +0100

    text image fix

--- elements/divider/templates/uikit.php
+++ elements/divider/templates/uikit.php
@@ -4,25 +4,138 @@
  * @var array $elementData
  */
 
-$style = $elementData['style'] ?? 'solid';
+$style = $elementData['style'] ?? 'simple';
+$icon = $elementData['icon'] ?? 'fa fa-star';
+$text = $elementData['text'] ?? '';
+$color = $elementData['color'] ?? 'default';
+$width = $elementData['width'] ?? 'full';
+$spacingTop = $elementData['spacing_top'] ?? 'medium';
+$spacingBottom = $elementData['spacing_bottom'] ?? 'medium';
 
-// UIkit Divider
-$classes = ['uk-divider'];
+// Width Mapping
+$widthMap = [
+    'full' => '100%',
+    'wide' => '80%',
+    'medium' => '60%',
+    'narrow' => '40%'
+];
+$widthStyle = $widthMap[$width] ?? '100%';
 
-// Style mapping
-if ($style === 'icon') {
-    $classes[] = 'uk-divider-icon';
-} elseif ($style === 'vertical') {
-    $classes[] = 'uk-divider-vertical';
-} elseif ($style === 'small') {
-    $classes[] = 'uk-divider-small';
-}
+// Spacing Mapping (UIkit utility classes)
+$spacingMapTop = [
+    'small' => 'uk-margin-small-top',
+    'medium' => 'uk-margin-top',
+    'large' => 'uk-margin-large-top'
+];
+$spacingMapBottom = [
+    'small' => 'uk-margin-small-bottom',
+    'medium' => 'uk-margin-bottom',
+    'large' => 'uk-margin-large-bottom'
+];
 
-$classStr = implode(' ', $classes);
+$marginClasses = [];
+$marginClasses[] = $spacingMapTop[$spacingTop] ?? 'uk-margin-top';
+$marginClasses[] = $spacingMapBottom[$spacingBottom] ?? 'uk-margin-bottom';
 
-// Vertical divider braucht spezielles Markup
-if ($style === 'vertical') {
-    echo '<span class="' . $classStr . '"></span>';
-} else {
-    echo '<hr class="' . $classStr . '">';
+// Color Mapping
+$colorMap = [
+    'default' => '#e5e5e5',
+    'primary' => 'var(--uk-primary, #1e87f0)',
+    'secondary' => 'var(--uk-secondary, #222)',
+    'success' => '#32d296',
+    'warning' => '#faa05a',
+    'danger' => '#f0506e'
+];
+$lineColor = $colorMap[$color] ?? $colorMap['default'];
+
+// Container für zentrierte Ausrichtung bei nicht-100% Breite
+$needsContainer = ($width !== 'full');
+$containerStart = $needsContainer ? '<div class="uk-flex uk-flex-center">' : '';
+$containerEnd = $needsContainer ? '</div>' : '';
+
+// Wrapper-Klassen
+$wrapperClasses = implode(' ', $marginClasses);
+
+switch ($style) {
+    case 'simple':
+        echo '<div class="' . $wrapperClasses . '">';
+        echo $containerStart;
+        echo '<hr class="uk-hr" style="width: ' . $widthStyle . '; border-top-color: ' . $lineColor . ';">';
+        echo $containerEnd;
+        echo '</div>';
+        break;
+
+    case 'double':
+        echo '<div class="' . $wrapperClasses . '">';
+        echo $containerStart;
+        echo '<div style="width: ' . $widthStyle . '; border-top: 1px solid ' . $lineColor . '; border-bottom: 1px solid ' . $lineColor . '; height: 4px;"></div>';
+        echo $containerEnd;
+        echo '</div>';
+        break;
+
+    case 'dotted':
+        echo '<div class="' . $wrapperClasses . '">';
+        echo $containerStart;
+        echo '<hr style="width: ' . $widthStyle . '; border: none; border-top: 2px dotted ' . $lineColor . ';">';
+        echo $containerEnd;
+        echo '</div>';
+        break;
+
+    case 'dashed':
+        echo '<div class="' . $wrapperClasses . '">';
+        echo $containerStart;
+        echo '<hr style="width: ' . $widthStyle . '; border: none; border-top: 2px dashed ' . $lineColor . ';">';
+        echo $containerEnd;
+        echo '</div>';
+        break;
+
+    case 'thick':
+        echo '<div class="' . $wrapperClasses . '">';
+        echo $containerStart;
+        echo '<hr style="width: ' . $widthStyle . '; border: none; border-top: 4px solid ' . $lineColor . ';">';
+        echo $containerEnd;
+        echo '</div>';
+        break;
+
+    case 'gradient':
+        echo '<div class="' . $wrapperClasses . '">';
+        echo $containerStart;
+        echo '<hr style="width: ' . $widthStyle . '; border: none; height: 2px; background: linear-gradient(90deg, transparent, ' . $lineColor . ', transparent);">';
+        echo $containerEnd;
+        echo '</div>';
+        break;
+
+    case 'icon':
+        // UIkit Divider mit Icon
+        echo '<div class="' . $wrapperClasses . '">';
+        echo $containerStart;
+        echo '<div style="width: ' . $widthStyle . ';">';
+        echo '<hr class="uk-divider-icon">';
+        echo '</div>';
+        echo $containerEnd;
+        echo '</div>';
+        break;
+
+    case 'text':
+        // Linie mit Text in der Mitte
+        echo '<div class="' . $wrapperClasses . ' uk-flex uk-flex-middle" style="width: ' . $widthStyle . '; margin-left: auto; margin-right: auto;">';
+        echo '<hr class="uk-hr uk-flex-1" style="border-top-color: ' . $lineColor . ';">';
+        echo '<span class="uk-margin-small-left uk-margin-small-right uk-text-muted">' . rex_escape($text) . '</span>';
+        echo '<hr class="uk-hr uk-flex-1" style="border-top-color: ' . $lineColor . ';">';
+        echo '</div>';
+        break;
+
+    case 'scroll':
+        // Scroll-Animation mit Chevron
+        echo '<div class="' . $wrapperClasses . ' uk-text-center">';
+        echo '<a href="#" uk-scroll class="uk-icon-button" uk-icon="icon: chevron-down; ratio: 1.5"></a>';
+        echo '</div>';
+        break;
+
+    default:
+        // Fallback: Einfache Linie
+        echo '<div class="' . $wrapperClasses . '">';
+        echo '<hr class="uk-hr">';
+        echo '</div>';
+        break;
 }
--- elements/text_image/templates/uikit.php
+++ elements/text_image/templates/uikit.php
@@ -5,41 +5,140 @@
  */
 
 $headline = $elementData['headline'] ?? '';
+$headlineTag = $elementData['headline_tag'] ?? 'h2';
 $text = $elementData['text'] ?? '';
 $image = $elementData['image'] ?? '';
-$imagePosition = $elementData['image_position'] ?? 'right';
+$imageAlt = $elementData['image_alt'] ?? $headline;
+$imageRatio = $elementData['image_ratio'] ?? 'auto';
+$layout = $elementData['layout'] ?? 'image_text';
+$spacing = $elementData['spacing'] ?? 'default';
+
+// Link
+$linkType = $elementData['link_type'] ?? '';
+$linkUrl = $elementData['link_url'] ?? '';
+$linkInternal = $elementData['link_internal'] ?? '';
+$linkText = $elementData['link_text'] ?? 'Mehr erfahren';
+$linkTarget = ($elementData['link_target'] ?? '_self') === '_blank' ? ' target="_blank"' : '';
+
+// Link URL ermitteln
+$href = '';
+if ($linkType === 'external' && $linkUrl) {
+    $href = $linkUrl;
+} elseif ($linkType === 'internal' && $linkInternal) {
+    $href = rex_getUrl($linkInternal);
+}
+
+// Bild URL via Media Manager
+$imageSrc = '';
+if ($image) {
+    $imageSrc = rex_media_manager::getUrl('content_text_image', $image);
+}
+
+// Spacing Mapping
+$spacingMap = [
+    'default' => '',
+    'compact' => 'uk-grid-small',
+    'spacious' => 'uk-grid-large'
+];
+$gridSpacing = $spacingMap[$spacing] ?? '';
+
+// Horizontale Layouts (links/rechts)
+$isHorizontal = in_array($layout, ['image_text', 'text_image']);
+
+// Grid Klassen
+$gridClasses = ['uk-grid-match', 'uk-flex-middle'];
+if ($gridSpacing) {
+    $gridClasses[] = $gridSpacing;
+}
+$gridClassStr = implode(' ', $gridClasses);
+
+// Bild mit optionalem Ratio rendern
+$renderImage = function($imageSrc, $imageAlt, $imageRatio) {
+    if (empty($imageSrc)) return '';
+    
+    $output = '';
+    
+    if ($imageRatio && $imageRatio !== 'auto') {
+        // Ratio-Container mit padding-bottom Trick für festes Seitenverhältnis
+        $ratioMap = [
+            '1-1' => '100%',      // 1:1
+            '4-3' => '75%',       // 3/4 = 75%
+            '16-9' => '56.25%',   // 9/16 = 56.25%
+            '21-9' => '42.86%'    // 9/21 = 42.86%
+        ];
+        $paddingBottom = $ratioMap[$imageRatio] ?? '56.25%';
+        
+        $output .= '<div class="uk-inline-clip uk-transition-toggle" style="position: relative; width: 100%; padding-bottom: ' . $paddingBottom . '; overflow: hidden;">';
+        $output .= '<img src="' . $imageSrc . '" alt="' . rex_escape($imageAlt) . '" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover;">';
+        $output .= '</div>';
+    } else {
+        $output .= '<img src="' . $imageSrc . '" alt="' . rex_escape($imageAlt) . '" class="uk-width-1-1">';
+    }
+    
+    return $output;
+};
+
+// Text-Content rendern
+$renderContent = function($headline, $headlineTag, $text, $href, $linkText, $linkTarget) {
+    $output = '';
+    
+    if ($headline) {
+        $output .= '<' . $headlineTag . ' class="uk-margin-small-bottom">' . rex_escape($headline) . '</' . $headlineTag . '>';
+    }
+    if ($text) {
+        $output .= '<div class="uk-text-default">' . $text . '</div>';
+    }
+    if ($href && $linkText) {
+        $output .= '<p class="uk-margin-top"><a href="' . $href . '"' . $linkTarget . ' class="uk-button uk-button-default">' . rex_escape($linkText) . '</a></p>';
+    }
+    
+    return $output;
+};
+
+$imageHtml = $renderImage($imageSrc, $imageAlt, $imageRatio);
+$contentHtml = $renderContent($headline, $headlineTag, $text, $href, $linkText, $linkTarget);
 ?>
 
 <div class="text-image-element">
-    <div class="uk-grid" data-uk-grid>
-        <?php if ($imagePosition === 'left'): ?>
-            <div class="uk-width-1-2@m">
-                <?php if ($image): ?>
-                    <img src="<?= rex_media_manager::getUrl('content_text_image', $image) ?>" alt="<?= rex_escape($headline) ?>" class="uk-width-1-1">
-                <?php endif; ?>
+    <?php if ($isHorizontal): ?>
+        <!-- Horizontales Layout -->
+        <div class="<?= $gridClassStr ?>" uk-grid>
+            <?php if ($layout === 'image_text'): ?>
+                <!-- Bild links, Text rechts -->
+                <div class="uk-width-1-2@m">
+                    <?= $imageHtml ?>
+                </div>
+                <div class="uk-width-1-2@m">
+                    <?= $contentHtml ?>
+                </div>
+            <?php else: ?>
+                <!-- Text links, Bild rechts -->
+                <div class="uk-width-1-2@m">
+                    <?= $contentHtml ?>
+                </div>
+                <div class="uk-width-1-2@m">
+                    <?= $imageHtml ?>
+                </div>
+            <?php endif; ?>
+        </div>
+    <?php else: ?>
+        <!-- Vertikales Layout (oben/unten) -->
+        <?php if ($layout === 'image_top'): ?>
+            <!-- Bild oben, Text unten -->
+            <div class="uk-margin-bottom">
+                <?= $imageHtml ?>
             </div>
-            <div class="uk-width-1-2@m">
-                <?php if ($headline): ?>
-                    <h2 class="uk-heading-medium"><?= rex_escape($headline) ?></h2>
-                <?php endif; ?>
-                <?php if ($text): ?>
-                    <div class="text-content"><?= $text ?></div>
-                <?php endif; ?>
+            <div>
+                <?= $contentHtml ?>
             </div>
         <?php else: ?>
-            <div class="uk-width-1-2@m">
-                <?php if ($headline): ?>
-                    <h2 class="uk-heading-medium"><?= rex_escape($headline) ?></h2>
-                <?php endif; ?>
-                <?php if ($text): ?>
-                    <div class="text-content"><?= $text ?></div>
-                <?php endif; ?>
+            <!-- Text oben, Bild unten -->
+            <div class="uk-margin-bottom">
+                <?= $contentHtml ?>
             </div>
-            <div class="uk-width-1-2@m">
-                <?php if ($image): ?>
-                    <img src="<?= rex_media_manager::getUrl('content_text_image', $image) ?>" alt="<?= rex_escape($headline) ?>" class="uk-width-1-1">
-                <?php endif; ?>
+            <div>
+                <?= $imageHtml ?>
             </div>
         <?php endif; ?>
-    </div>
+    <?php endif; ?>
 </div>
