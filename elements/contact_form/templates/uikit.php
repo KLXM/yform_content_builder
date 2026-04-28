07c89629e270b279ce4000665833f21bbee8253c
Thomas Skerbis <thomas.skerbis@mac.com>
2026-04-29 00:33:11 +0200

    remove: contact_form Element entfernt, doform2026 ist der Ersatz

--- elements/contact_form/templates/uikit.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Kontaktformular - Template (Alias)
- * Delegiert an doform2026 - das neue Haupt-Formular-Element.
- *
- * @var array $elementData
- */
-include rex_path::addon('yform_content_builder', 'elements/doform2026/templates/uikit.php');
